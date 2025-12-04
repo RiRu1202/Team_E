1d7e5ea52d6ca203c64b3272712587a56deaebaa
nisimotoiori <h1202515017@hiratagakuen.onmicrosoft.com>
2025-12-04 10:43:19 +0900

    西本_マージしたのでテスト

---
+++ Tesm_E/Assets/SakagutiRiku/sprict/Stage1Goal_s.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Stage1Goal_t : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerPrefs.SetInt("ClearedStage1", 1);
+            FindObjectOfType<SaveLoadManager_t>().SaveScene("Test_sakagiti");
+            SceneManager.LoadScene("Clear");
+        }
+    }
+}
