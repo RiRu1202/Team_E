2f3a78e2bbdb401c460e4c7db2eced32b9b251ad
sakagutiriku <h1202515020@hiratagakuen.onmicrodoft.com>
2025-12-04 12:28:24 +0900

    test_tanaka プレイヤー完成

--- Tesm_E/Assets/tanakamiko/Scripts/EnemyDeath_t.cs
+++ Tesm_E/Assets/tanakamiko/Scripts/EnemyDeath_t.cs
@@ -4,7 +4,7 @@ public class EnemyDeath_t:MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("PlayerAttack"))
+        if (collision.gameObject.CompareTag("PlayerAttack")||collision.gameObject.CompareTag("PlayerwaterAttack"))
         {
             Destroy(gameObject);
             Destroy(collision.gameObject);
