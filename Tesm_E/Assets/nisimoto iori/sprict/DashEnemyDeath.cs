5d85aa43253bca2d8c544b9237c7b1ef07693df1
MikoTanaka <h1202519004@hiratagakuen.onmicrosoft.com>
2025-12-08 11:41:29 +0900

    u

--- Tesm_E/Assets/nisimoto iori/sprict/DashEnemyDeath.cs
+++ Tesm_E/Assets/nisimoto iori/sprict/DashEnemyDeath.cs
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-・ｿusing UnityEngine;
- public class DashEnemyDeath : MonoBehaviour
-{[Header("遘ｻ蜍戊ｨｭ螳・)] public float speed = 2f; // 蟾ｦ縺ｫ遘ｻ蜍輔☆繧九せ繝斐・繝・
-private bool isDead = false; // 謨ｵ縺悟偵＆繧後◆縺九←縺・°縺ｮ繝輔Λ繧ｰ
-void Start() { // Rigidbody2D 繧貞叙蠕・
-  Rigidbody2D rb = GetComponent<Rigidbody2D>(); 
-       if (rb != null) { // 驥榊鴨繧堤┌蜉ｹ蛹悶＠縺ｦ遨ｺ荳ｭ縺ｫ豬ｮ縺九○繧具ｼ郁誠縺｡縺ｪ縺・ｈ縺・↓・・
-        rb.gravityScale = 1;
-       }
-=======
+
 using UnityEngine;
 
 public class DashEnemyDeath : MonoBehaviour
@@ -22,7 +12,6 @@ public class DashEnemyDeath : MonoBehaviour
         { // 重力を無効化して空中に浮かせる（落ちないように）
             rb.gravityScale = 1;
         }
->>>>>>> 101f0fd386128ccc887f0f33c258cff8af10126d
     }
     void Update()
     { // 倒されていないときだけ移動処理を行う
